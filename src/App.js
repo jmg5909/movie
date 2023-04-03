3b3b6c358bb4c47f919483d51d57184e906bbf96
jmg5909 <jmg5909@naver.com>
2023-04-03 17:19:03 +0900

    영화정보 출력하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8")
+    .then((response) => response.json());
+    setMovies(json.data.movies);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+    console.log(movies);
+  }, []); // 한번만 실행 : 빈 의존성 배열을 지정
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>movies({movies.length})</h1>
+      {isLoading ? (
+        "로딩 중..."
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <img src={movie.large_cover_image} alt={movie.title} />
+              <h2>{movie.title}</h2>
+              <h2>평점</h2>
+              <p>{movie.rating} / 10</p>
+              <h2>장르</h2>
+              <div>
+                {movie.genres.map((genre, i) => (
+                  <span key={i}>{genre}</span>
+                ))}
+              </div>
+              <h2>줄거리</h2>
+              <p>{movie.summary}</p>
+              </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
